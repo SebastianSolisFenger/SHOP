84c4ba76846381f0c04d38ff651552b5f0fead33
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-08-04 13:32:56 -0300

    now commit previous one

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,38 @@
-import Home from './pages/Home';
+// @ts-nocheck
+import Home from './pages/Home.jsx';
+import ProductList from './pages/ProductList.jsx';
+import Product from './pages/Product.jsx';
+import Cart from './pages/Cart.jsx';
+import Login from './pages/Login.jsx';
+import Register from './pages/Register.jsx';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from 'react-router-dom';
 
 const App = () => {
-  return <Home />;
+  const user = true;
+  return (
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/products/:category" element={<ProductList />} />
+        <Route path="/product/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route
+          path="/login"
+          element={user ? <Navigate replace to={'/'} /> : <Login />}
+        />
+        {/* <Route path="/login">{user ? <Navigate to="/" /> : <Login />}</Route> */}
+        <Route
+          path="/register"
+          element={user ? <Navigate replace to={'/'} /> : <Register />}
+        />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
