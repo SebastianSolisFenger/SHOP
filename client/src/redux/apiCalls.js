a8589708c951fd03593e114cf8f15e1ce7a897de
Sebastian Daniel Solis Fenger <solisfenger@gmail.com>
2022-08-05 00:37:29 -0300

    lastest commit 8/5/22

---
+++ client/src/redux/apiCalls.js
@@ -0,0 +1,17 @@
+export const login = async (dispatch, user) => {
+  // dispatch(loginStart());
+  // try {
+  //     const response = await fetch('/api/login', {
+  //     method: 'POST',
+  //     body: JSON.stringify(user),
+  //     });
+  //     const data = await response.json();
+  //     if (data.error) {
+  //     dispatch(loginFailure(data.error));
+  //     } else {
+  //     dispatch(loginSuccess(data));
+  //     }
+  // } catch (error) {
+  //     dispatch(loginFailure(error));
+  // }
+};
